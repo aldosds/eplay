d90ef27f832e512e03c392b8d2d6479b864ae905
Aldo Santos <aldoacesso@gmail.com>
2025-02-20 11:26:21 -0300

    feat(Cart and products): Created the cart and functionality to add and remove products

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,48 @@
 import { Link } from 'react-router-dom'
 
-import { HeaderBar, LInkItem, LinkCart, Links } from './styles'
+import { HeaderBar, LInkItem, CartButton, Links } from './styles'
 
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
 
-const Header = () => (
-  <HeaderBar>
-    <div>
-      <Link to="/">
-        <img src={logo} alt="EPLAY" />
-      </Link>
-      <nav>
-        <Links>
-          <LInkItem>
-            <Link to="/categories">Categorias</Link>
-          </LInkItem>
-          <LInkItem>
-            <a href="#">Novidades</a>
-          </LInkItem>
-          <LInkItem>
-            <a href="#">Promoções</a>
-          </LInkItem>
-        </Links>
-      </nav>
-    </div>
-    <LinkCart href="#">
-      0 - produtos(s)
-      <img src={carrinho} alt="Carrinho" />
-    </LinkCart>
-  </HeaderBar>
-)
+import { open } from '../../store/reducers/cart'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
+const Header = () => {
+  const dispatch = useDispatch()
+  const { items } = useSelector((state: RootReducer) => state.cart)
+
+  const openCart = () => {
+    dispatch(open())
+  }
+
+  return (
+    <HeaderBar>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="EPLAY" />
+        </Link>
+        <nav>
+          <Links>
+            <LInkItem>
+              <Link to="/categories">Categorias</Link>
+            </LInkItem>
+            <LInkItem>
+              <a href="#">Novidades</a>
+            </LInkItem>
+            <LInkItem>
+              <a href="#">Promoções</a>
+            </LInkItem>
+          </Links>
+        </nav>
+      </div>
+      <CartButton onClick={openCart}>
+        {items.length} - produtos(s)
+        <img src={carrinho} alt="Carrinho" />
+      </CartButton>
+    </HeaderBar>
+  )
+}
 
 export default Header
